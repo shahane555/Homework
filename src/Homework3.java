00ff5ef52c26774bcc41f5d894f32178b4c630cb
Shahane <shahane.manucharyan@mail.ru>
2023-01-05 12:04:29 +0400

    Add 2 tasks to Homework5

---
+++ src/Homework3.java
@@ -0,0 +1,43 @@
+public class Homework3 {
+
+    public static void main(String[] args) {
+//      1st task
+        int c;
+        int a = 55;
+        int b = 24;
+        c = a * a + b * b;
+        System.out.println("c = a²+ b² = " + c);
+        System.out.println();
+
+//        2nd task
+        a = 5; //top basis of the trapezoid
+        b = 9; //bottom basis of the trapezoid
+        c = 7; //height of the trapezoid
+        int area = (a + b) / 2 * c; //area of trapezoid
+        System.out.println("Area of the trapezoid is " + area + " cm²");
+        System.out.println();
+
+//        3rd task
+        int number = 354;
+        int thirdDigit = number % 10;
+        number = number / 10;
+        int secondDigit = number % 10;
+        number = number / 10;
+        int firstDigit = number;
+        System.out.println("All digits of the current number are");
+        System.out.println(firstDigit);
+        System.out.println(secondDigit);
+        System.out.println(thirdDigit);
+
+
+        byte b1 = 100;
+        byte c1 =26;
+        byte y = (byte) (b1 + c1);
+        System.out.println(y);
+        double d = 5.2;
+        System.out.println((double) (5/2));
+        System.out.println(d/2);
+
+
+    }
+}
--- src/Homework5.java
+++ src/Homework5.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Homework5 {
+import java.util.Scanner;
+
+public class Homework5 {
+    public static void main(String[] args) {
+//        __________1st task___________
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        if (a + b > c && b + c > a && a + c > b) {
+            System.out.println("Triangle with " + a + ", " + b + ", " + c + " sides is exist.");
+        } else {
+            System.out.println("Triangle with " + a + ", " + b + ", " + c + " sides doesn't exist.");
+        }
+
+//       ___________2nd task____________
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Enter a number between 1 to 12");
+        int month = scan1.nextInt();
+        String[] arr = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12 -> System.out.println(arr[month - 1] + " has 31 days.");
+            case 4, 6, 9, 11 -> System.out.println(arr[month - 1] + " has 30 days.");
+            case 2 -> System.out.println(arr[month - 1] + " has 28 or 29 days.");
+            default -> {
+                System.out.println("Invalid number! Enter a valid number between 1 to 12");
+                System.exit(1);
+            }
+        }
+
+    }
 }
+
