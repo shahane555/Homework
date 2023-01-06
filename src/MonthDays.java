32dd358fbca442c0c1e50eb741fde4daccbd4e97
Shahane <shahane.manucharyan@mail.ru>
2023-01-06 15:48:54 +0400

    Write a program displaying the count of the days of the coresponding month

--- src/MonthDays.java
+++ src/MonthDays.java
@@ -5,11 +5,10 @@ public class MonthDays {
         Scanner scan1 = new Scanner(System.in);
         System.out.println("Enter a number between 1 to 12");
         int month = scan1.nextInt();
-        String[] arr = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         switch (month) {
-            case 1, 3, 5, 7, 8, 10, 12 -> System.out.println(arr[month - 1] + " has 31 days.");
-            case 4, 6, 9, 11 -> System.out.println(arr[month - 1] + " has 30 days.");
-            case 2 -> System.out.println(arr[month - 1] + " has 28 or 29 days.");
+            case 1, 3, 5, 7, 8, 10, 12 -> System.out.println(month + "th month" + " has 31 days.");
+            case 4, 6, 9, 11 -> System.out.println(month + "th month" + " has 30 days.");
+            case 2 -> System.out.println(month + "th month" + " has 28 or 29 days.");
             default -> {
                 System.out.println("Invalid number! Enter a valid number between 1 to 12");
                 System.exit(1);
---
+++ src/MonthDays2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MonthDays2 {
+    public static void main(String[] args) {
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Enter a number between 1 to 12");
+        int month = scan1.nextInt();
+        switch (month) {
+            case 1 -> System.out.println("January" + " has 31 days.");
+            case 2 -> System.out.println("February" + " has 28 or 29 days.");
+            case 3 -> System.out.println("March" + " has 31 days.");
+            case 4 -> System.out.println("April" + " has 30 days.");
+            case 5 -> System.out.println("May" + " has 31 days.");
+            case 6 -> System.out.println("June" + " has 30 days.");
+            case 7 -> System.out.println("July" + " has 31 days.");
+            case 8 -> System.out.println("August" + " has 31 days.");
+            case 9 -> System.out.println("September" + " has 30 days.");
+            case 10 -> System.out.println("October" + " has 31 days.");
+            case 11 -> System.out.println("November" + " has 30 days.");
+            case 12 -> System.out.println("December" + " has 31 days.");
+            default -> {
+                System.out.println("Invalid number! Enter a valid number between 1 to 12");
+                System.exit(1);
+            }
+        }
+    }
+}
