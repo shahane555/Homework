6962f378eacd4146019a7eb83302d6df94f5c0fd
Shahane <shahane.manucharyan@mail.ru>
2023-01-10 16:40:55 +0400

    Write a program demonstrating with arrays without loops

---
+++ src/DoubleArray.java
@@ -0,0 +1,61 @@
+public class DoubleArray {
+
+    public static void main(String[] args) {
+        double[] array = {-5, -7, -9, 3, -6};// {-3, 8, 7, 2, -1}
+        double[] negative = new double[array.length]; //8, 29, 0, 0, 0
+        double[] positive = new double[array.length];
+        int i = 0;
+        int j = 0;
+        if (array[0] >= 0) {
+            positive[i] = array[0];
+            i++;
+        } else {
+            negative[j] = array[0];
+            j++;
+        }
+        if (array[1] >= 0) {
+            positive[i] = array[1];
+            i++;
+        } else {
+            negative[j] = array[1];
+            j++;
+        }
+        if (array[2] >= 0) {
+            positive[i] = array[2];
+            i++;
+        } else {
+            negative[j] = array[2];
+            j++;
+        }
+        if (array[3] >= 0) {
+            positive[i] = array[3];
+            i++;
+        } else {
+            negative[j] = array[3];
+            j++;
+        }
+        if (array[4] >= 0) {
+            positive[i] = array[4];
+            i++;
+        } else {
+            negative[j] = array[4];
+            j++;
+        }
+        System.out.print(positive[0] + ", ");
+        System.out.print(positive[1] + ", ");
+        System.out.print(positive[2] + ", ");
+        System.out.print(positive[3] + ", ");
+        System.out.print(positive[4] + ", ");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
