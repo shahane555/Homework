6f26d8c412da915d5432cd4fe8eab1e1428d9060
Shahane <shahane.manucharyan@mail.ru>
2023-01-10 14:42:17 +0400

    Write a code for minute scale

---
+++ src/Clock.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Clock {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 12");
+        int number = scan.nextInt();
+        if (number < 1 || number > 12) {
+            System.out.println("Invalid number! Enter valid number between 1 and 12!");
+            System.exit(1);
+        } else
+            System.out.println("Corresponding minutes: " + 5 * number);
+    }
+}
