8420afd3fb73832ef8c0f4c5100a13f3e17a183b
shahane <shahane.manucharyan@mail.ru>
2023-01-18 01:52:03 +0400

    Combine array elements as a binary number and convert it to decimal

---
+++ src/BinaryToDecimal.java
@@ -0,0 +1,14 @@
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 1, 0, 1, 1, 0, 1};
+        int decimalResult = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                arr[arr.length - 1 - i] = 2 * arr[arr.length - 1 - i];
+            }
+            decimalResult += arr[arr.length - 1 - i];
+        }
+        System.out.println(decimalResult);
+    }
+
+}
