f5fdcb3cf7af12e18488750f570adb6d13eea017
shahane <shahane.manucharyan@mail.ru>
2023-01-29 19:17:38 +0400

    Write a method to return different values

--- src/Part2/Exponent.java
+++ src/Part2/Exponent.java
@@ -1,2 +1,26 @@
-package Part2;public class Exponent {
+package Part2;
+
+public class Exponent {
+    public static void main(String[] args) {
+        double c = exponent(4, -2);
+        System.out.println("c = " + c);
+    }
+
+    static double exponent(int a, int b) {
+        if (a > 10 || -a > 10 || b > 10 || -b > 10) {
+            return -1;
+        } else if (a < 0) {
+            return -2;
+        } else {
+            double mult = 1;
+            for (int i = 0; i < b; i++) {
+                mult *= a;
+            }
+            for (int i = 0; i < -b; i++) {
+                mult *= (double) 1 / a;
+            }
+            return mult;
+        }
+    }
 }
+
