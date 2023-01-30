00b559ff9f58f52b101d8d8494adfd147bb6648d
shahane <shahane.manucharyan@mail.ru>
2023-01-31 00:53:58 +0400

    Add new sets of parameters

--- src/Part2/Exponent.java
+++ src/Part2/Exponent.java
@@ -2,14 +2,20 @@ package Part2;
 
 public class Exponent {
     public static void main(String[] args) {
-        double c = exponent(4, -2);
-        System.out.println("c = " + c);
+        System.out.println(exponent(4, -2));
+        System.out.println(exponent(-3, 3));
+        System.out.println(exponent(5, 11));
+        System.out.println(exponent(14, -3));
+        System.out.println(exponent(7, 0));
+        System.out.println(exponent(10, -2));
+        System.out.println(exponent(0, -6));
+        System.out.println(exponent(7, 3));
     }
 
     static double exponent(int a, int b) {
         if (a > 10 || -a > 10 || b > 10 || -b > 10) {
             return -1;
-        } else if (a < 0) {
+        } else if (a <= 0) {
             return -2;
         } else {
             double mult = 1;
@@ -23,4 +29,3 @@ public class Exponent {
         }
     }
 }
-
