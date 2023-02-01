6ee243aa0c928f478c75a90456759c061f177c1d
shahane <shahane.manucharyan@mail.ru>
2023-02-01 17:30:50 +0400

    Check if current number Armstrong or not

--- src/Part2/Armstrong.java
+++ src/Part2/Armstrong.java
@@ -1,2 +1,35 @@
-package Part2;public class Armstrong {
+package Part2;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        if (isArmstrong(548834)) {
+            System.out.println("Armstrong number");
+        } else {
+            System.out.println("Not Armstrong number");
+        }
+    }
+
+    static int lengthOfNum(int a) {
+        int counter = 0;
+        while (a > 0) {
+            a = a / 10;
+            counter++;
+        }
+        return counter;
+    }
+    static boolean isArmstrong(int a) {
+        int digit = 0;
+        int result = 0;
+        int copyOfa = a;
+        while (copyOfa != 0) {
+            int pow = 1;
+            digit = copyOfa % 10;
+            copyOfa = copyOfa / 10;
+            for (int i = 0; i < lengthOfNum(a); i++) {
+                pow *= digit;
+            }
+            result += pow;
+        }
+        return result == a;
+    }
 }
