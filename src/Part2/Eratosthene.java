f32f407fade2032b327803f22ab15b4601bbc3f0
shahane <shahane.manucharyan@mail.ru>
2023-02-03 17:46:08 +0400

    Write an algorithm displaying prime numbers

---
+++ src/Part2/Eratosthene.java
@@ -0,0 +1,31 @@
+package Part2;
+
+public class Eratosthene {
+    public static void main(String[] args) {
+        primeNumbers(70);
+    }
+    static void primeNumbers(int n) {
+        boolean[] arr = new boolean[n + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = true;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (arr[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    arr[j] = false;
+                }
+            }
+        }
+        for (int k = 2; k <= n; k++) {
+            if (arr[k])
+                System.out.print(k + " ");
+        }
+    }
+}
+
+
+
+
+
+
+
