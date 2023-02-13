450f0374b5f2f8da56d260f845dd5a586776d538
shahane <shahane.manucharyan@mail.ru>
2023-02-14 00:52:12 +0400

    Create 2 new classes

--- src/part2/GasStove.java
+++ src/part2/GasStove.java
@@ -1,2 +1,63 @@
-package part2;public class GasStove {
+package part2;
+
+public class GasStove {
+    public static void main(String[] args) {
+        Stove newStove = new Stove(1, 3, "Glass", "Electrolux");
+        newStove.electricBurnerOn = true;
+        newStove.gasBurnerOn = true;
+        if (newStove.getGasStoveEnergy() == 0) {
+            System.out.println("To get the total energy generated from the gas stove switch on all electric burners and all gas burners");
+        } else {
+            System.out.println("The total energy generated from the gas stove is " + newStove.getGasStoveEnergy());
+        }
+    }
+}
+
+class Stove {
+    public boolean electricBurnerOn = false;
+    public boolean gasBurnerOn = false;
+    private int electricBurner;
+    private int gasBurner;
+    String panelCover;
+    String manufacturer;
+
+    public Stove(int electricBurner, int gasBurner, String panelCover, String manufacturer) {
+        setElectricBurner(electricBurner);
+        setGasBurner(gasBurner);
+        this.panelCover = panelCover;
+        this.manufacturer = manufacturer;
+    }
+
+    public void setElectricBurner(int electricBurner) {
+        if (electricBurner >= 1 && electricBurner <= 3) {
+            this.electricBurner = electricBurner;
+        } else {
+            System.out.println("Invalid input, gas stove should have minimum 1, maximum 3 electric burners");
+            System.exit(1);
+        }
+    }
+
+    public void setGasBurner(int gasBurner) {
+        if (gasBurner >= 3 && gasBurner <= 5) {
+            this.gasBurner = gasBurner;
+        } else {
+            System.out.println("Invalid input, gas stove should have minimum 3, maximum 5 gas burners");
+            System.exit(1);
+        }
+    }
+
+    public int getElectricBurner() {
+        return electricBurner;
+    }
+
+    public int getGasBurner() {
+        return gasBurner;
+    }
+
+    public int getGasStoveEnergy() {
+        if (electricBurnerOn && gasBurnerOn) {
+            return getElectricBurner() * 100 + getGasBurner() * 80;
+        }
+        return 0;
+    }
 }
--- src/part2/Sport.java
+++ src/part2/Sport.java
@@ -1,2 +1,46 @@
-package part2;public class Sport {
+package part2;
+
+public class Sport {
+    public static void main(String[] args) {
+        Sportsman sportsman1 = new Sportsman(55, 63, 95, 84, 70);
+        Sportsman sportsman2 = new Sportsman(54, 55, 80, 94, 100, 88, 70);
+        Sportsman sportsman3 = new Sportsman(50, 60, 70);
+        Sportsman sportsman4 = new Sportsman(55, 55);
+        System.out.println("Average points collected by the sportsmans below are ");
+        double[] arr = {sportsman1.getAverageOfPoints(), sportsman2.getAverageOfPoints(), sportsman3.getAverageOfPoints(), sportsman4.getAverageOfPoints()};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Sportsman No" + (i + 1) + " " + arr[i]);
+        }
+    }
+}
+
+class Sportsman {
+    private int[] collectedPoints;
+
+    Sportsman(int... collectedPoints) {
+        setCollectedPoints(collectedPoints);
+    }
+
+    public void setCollectedPoints(int... collectedPoints) {
+        int counter = 0;
+        for (int i = 0; i < collectedPoints.length; i++) {
+            if (collectedPoints[i] >= 50 && collectedPoints[i] <= 100) {
+                counter++;
+            }
+        }
+        if (counter == collectedPoints.length) {
+            this.collectedPoints = collectedPoints;
+        } else {
+            System.out.println("One of your inputs is out of [50, 100] range");
+            System.exit(1);
+        }
+    }
+
+    public double getAverageOfPoints() {
+        double result = 0;
+        for (int i = 0; i < collectedPoints.length; i++) {
+            result += collectedPoints[i];
+        }
+        return result / collectedPoints.length;
+    }
 }
