de24bbf1d10f18d023b2c7c55dc1dac7a9734f3d
shahane <shahane.manucharyan@mail.ru>
2023-02-19 12:35:45 +0400

    Write a program to find GCD in array

--- src/GreatestCommonDivisor/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor/GreatestCommonDivisor.java
@@ -2,15 +2,15 @@ package GreatestCommonDivisor;
 
 public class GreatestCommonDivisor {
     public static void main(String[] args) {
-        int[] arr = {120, 36, 24, 240, 72, 120};
+        int[] arr = {120, 36, 24, -2, 72, 120};
         int min = 2147483647; //max int range
         int greatestCommonDivisor = 1;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
+            if (arr[i] < min && -arr[i] < min) {
                 min = arr[i];
             }
         }
-        for (int i = 1; i <= min; i++) {
+        for (int i = 1; i <= min || i <= -min; i++) {
             int count = 0;
             for (int j = 0; j < arr.length; j++) {
                 if (arr[j] % i == 0) {
