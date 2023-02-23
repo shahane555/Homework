170d0549f8f20f778e9416d729f1ac160179b586
shahane <shahane.manucharyan@mail.ru>
2023-02-23 17:27:11 +0400

    Sort current array

---
+++ src/array/SortedArray.java
@@ -0,0 +1,26 @@
+package array;
+
+public class SortedArray {
+    public static void main(String[] args) {
+        int[] array = {-23, 5, 12, 5, 5, -23, 12, 5, 12, -23, 5, 5, 12, 5, -23, 12, 12, -23};
+        int[] sortedArray = new int[array.length];
+        int[] arr = {-23, 5, 12};
+        int repeatedNumbers = 0;
+        int copyOfRepeatedNumbers = 0;
+        for (int i = 0; i < arr.length; i++) {
+            copyOfRepeatedNumbers += repeatedNumbers;
+            repeatedNumbers = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == arr[i]) {
+                    repeatedNumbers++;
+                }
+            }
+            for (int k = copyOfRepeatedNumbers; k < copyOfRepeatedNumbers + repeatedNumbers; k++) {
+                sortedArray[k] = arr[i];
+            }
+        }
+        for (int i = 0; i < sortedArray.length; i++) {
+            System.out.print(sortedArray[i] + " ");
+        }
+    }
+}
