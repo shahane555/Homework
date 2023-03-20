9309c372a709db1dbba4f59ab8a5b58625c9b653
shahane <shahane.manucharyan@mail.ru>
2023-03-20 17:52:27 +0400

    Add 4 new classes

--- src/card/Standard.java
+++ src/card/Standard.java
@@ -1,9 +1,9 @@
 package card;
 
-public class Standard extends Card{
-Standard(String cardHoldersName, String code, int amount) {
-    super(cardHoldersName, code, amount);
-}
+public class Standard extends Card {
+    Standard(String cardHoldersName, String code, int amount) {
+        super(cardHoldersName, code, amount);
+    }
 }
 
 
