80023ab159089cc331e46ed83f48af933369ae72
shahane <shahane.manucharyan@mail.ru>
2023-03-24 09:14:04 +0400

    Add some changes

--- src/card/Gold.java
+++ src/card/Gold.java
@@ -2,6 +2,7 @@ package card;
 
 public class Gold extends Card {
     private int amountForLunch;
+    private int bonusPoints;
 
     public Gold(String cardHoldersName, String code, int amount, int expirationYear) {
         super(cardHoldersName, code, amount, expirationYear);
@@ -18,6 +19,14 @@ public class Gold extends Card {
         } else {
             System.out.println("Error!");
             System.exit(1);
+        }
+    }
+
+    @Override
+    public void withdrawMoney(int money) {
+        if (money > 0 && money <= getAmount()) {
+            setAmount(getAmount() - money);
+            bonusPoints += 2;
         }
     }
 }
