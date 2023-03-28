13836a109005e41b3476c17f4869045a8f9c2ce4
shahane <shahane.manucharyan@mail.ru>
2023-03-28 15:55:00 +0400

    Add new classes for school workers

--- src/human/HeadTeacher.java
+++ src/human/HeadTeacher.java
@@ -1,9 +1,17 @@
 package human;
 
 public class HeadTeacher extends Teacher {
+    private double coefficient;
 
     public HeadTeacher(String name, int age, double salary, double coefficient) {
-        super(name, age, salary * coefficient);
+        super(name, age, salary);
+        if (coefficient >= 1.5 && coefficient <= 2) {
+            this.coefficient = coefficient;
+            setSalary(salary * coefficient);
+        } else {
+            System.out.println("Invalid coefficient");
+            System.exit(1);
+        }
     }
 }
 
