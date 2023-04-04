40615744f340ef4c64b2224ae9f4974e228b67cc
shahane <shahane.manucharyan@mail.ru>
2023-04-04 21:37:22 +0400

    Make some changes

--- src/electronics/Shop.java
+++ src/electronics/Shop.java
@@ -1,18 +1,19 @@
 package electronics;
 
-import card.Gold;
-
 public class Shop {
-    public static void main(String[] args) {
-        ElectronicDevices e = new DesktopComputer("LG", "29V950-A.AA5SU1", 450, 550, "8 GB", "Windows 10");
-        ElectronicDevices l = new Laptop("Lenovo", "8VUL0086", 300, 430, "4 GB", "Windows 11 S");
-        ElectronicDevices s = new Smartphone("Samsung", "Galaxy s23", 650, 760, "50 MP", "256 GB", "Android 13.0");
-        ElectronicDevices h = new Headphones("Philips", "H8506", 100, 150);
-        ElectronicDevices e2 = new Earbuds("Sony", "WF - 1000XM4", 80, 100);
+    private ElectronicDevices[] electronicDevices;
 
-        System.out.println(ElectronicDevices.totalIncome(e, l, s, h, e2));
-        double a = s.payWithCard(new Gold("Name Surname", "12345678", 1000, 2024));
-        System.out.println(a);
+    Shop(ElectronicDevices... electronicDevices) {
+        this.electronicDevices = electronicDevices;
+    }
+
+    public static void compareTwoDevices(ElectronicDevices obj1, ElectronicDevices obj2) {
+        if (obj1.getCostPrice() > obj2.getCostPrice()) {
+            System.out.println("The first device is more expensive than the second");
+        } else if (obj1.getCostPrice() < obj2.getCostPrice()) {
+            System.out.println("The first device is cheaper than the second");
+        } else {
+            System.out.println("Both devices have the same price");
+        }
     }
 }
-
